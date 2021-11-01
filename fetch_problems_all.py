6f776f7b33456c6c6629de558450baee13f5edbc
bunnyxt <bunnyxt@outlook.com>
2021-11-01 02:07:15 -0700

    feat: add get_csrftoken function

--- fetch_problems_all.py
+++ fetch_problems_all.py
@@ -1,10 +1,27 @@
 import urllib3
 import json
+import re
 
 
-def fetch_problems(limit=50):
+def get_csrftoken():
     http = urllib3.PoolManager()
-    cookie = 'csrftoken=fakecsrftokenfakecsrftokenfakecsrftokenfakecsrftokenfakecsrftoken'
+    r = http.request(
+        'GET',
+        'https://leetcode.com/problemset/all/',
+        redirect=False
+    )
+    if r.status != 302:
+        raise RuntimeError('Fail to get csrftoken! status: %d, data: %s' % (r.status, r.data))
+    match_obj = re.search('csrftoken=(\S+); ', r.headers['Set-Cookie'])
+    if match_obj:
+        return match_obj.group(1)
+    else:
+        raise RuntimeError('Fail to parse csrftoken from headers! headers: %s' % r.headers)
+
+
+def fetch_problems(csrftoken, limit=50):
+    http = urllib3.PoolManager()
+    cookie = 'csrftoken=%s' % csrftoken
     data = {
         'query': 'query problemsetQuestionList($categorySlug:String,$limit:Int,$skip:Int,$filters:QuestionListFilterInput){problemsetQuestionList:questionList(categorySlug:$categorySlug limit:$limit skip:$skip filters:$filters){total:totalNum questions:data{acRate difficulty freqBar frontendQuestionId:questionFrontendId isFavor paidOnly:isPaidOnly status title titleSlug topicTags{name id slug}hasSolution hasVideoSolution}}}',
         'variables': {
@@ -31,13 +48,17 @@ def fetch_problems(limit=50):
 
 
 def main():
+    print('Now try get csrftoken...')
+    csrftoken = get_csrftoken()
+    print('Got csrftoken %s.' % csrftoken)
+
     print('Now try get LeetCode problems total count...')
-    response_content = fetch_problems()
+    response_content = fetch_problems(csrftoken)
     total_count = response_content['data']['problemsetQuestionList']['total']
     print('Found %d problems in total.' % total_count)
 
     print('Now try fetch all %d LeetCode problems...' % total_count)
-    response_content = fetch_problems(limit=total_count)
+    response_content = fetch_problems(csrftoken, total_count)
     questions_all = {q['frontendQuestionId']: q for q in
                      response_content['data']['problemsetQuestionList']['questions']}
     print('All %d problems fetched.' % total_count)
