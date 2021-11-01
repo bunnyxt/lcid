790dba48c999d53dfd37f687d1e8ded165127e70
bunnyxt <bunnyxt@outlook.com>
2021-11-01 01:03:22 -0700

    feat: add flask backend

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect
+from flask_cors import CORS
+import json
+
+app = Flask(__name__, static_url_path='', static_folder='')
+CORS(app)
+
+# load problems
+with open('problems_all.json', 'r') as f:
+    problems_all_json = f.read()
+    problems_all = json.loads(problems_all_json)
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/<problem_id>')
+def go_redirect(problem_id):
+    problem_info = problems_all.get(problem_id, None)
+    if not problem_info:
+        return 'Fail to redirect to leetcode problem %s page.' % problem_id
+    return redirect('https://leetcode.com/problems/%s/' % problem_info['titleSlug'])
+
+
+@app.route('/info/<problem_id>')
+def info(problem_id):
+    problem_info = problems_all.get(problem_id, None)
+    if not problem_info:
+        return '{"code":404,"message":"Fail to get info of leetcode problem %s."}' % problem_id, 404
+    return json.dumps(problems_all[problem_id])
