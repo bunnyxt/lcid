0862084b9fe4b696d80969b1e146fdedbbd2d0f6
bunnyxt <bunnyxt@outlook.com>
2022-06-05 19:21:55 -0700

    feat: add problems table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import RedirectCard from './components/RedirectCard';
+import ProblemsTable from './components/ProblemsTable';
 
 const App = () => {
   return (
     <div className="App">
       <div className="redirect-wrapper">
         <RedirectCard />
+      </div>
+      <div className="problems-table-wrapper">
+        <ProblemsTable />
       </div>
     </div>
   );
